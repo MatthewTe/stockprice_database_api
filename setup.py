1b289f5f3570b3eb1197f144dd5e372d96b3970a
MatthewTe <teelucksingh.matthew1@gmail.com>
2020-06-19 12:27:35 -0400

    Added the setuptools to make package installable

---
+++ setup.py
@@ -0,0 +1,30 @@
+import setuptools
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="stockprice_db_api",
+    version="0.1.0",
+    description="A package that wraps the yfinance Yahoo Finance api in an api that interacts and maintains a sqlite database.",
+    long_description=long_description,
+    url="https://github.com/MatthewTe/stockprice_database_api",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Development Status :: 3 - Alpha ",
+        "Intended Audience :: Developers",
+        "Topic :: Data Science :: Pipeline API",
+        "Programming Language :: Python :: 3",
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir = {'':'stockprice_database_api'},
+    install_requires=['yfinance', 'pandas']
+
+)
